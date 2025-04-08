4de07ce9e8212b696b4ba039abd41a1b6d3ae5cf
Rosie9338 <embercorum@gmail.com>
2025-04-08 10:42:42 -0400

    Delete Project2-Django/budget_log directory

--- Project2-Django/budget_log/budget_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Transaction, Entry
-
-admin.site.register(Transaction)
-admin.site.register(Entry)
-# Register your models here.
--- Project2-Django/budget_log/budget_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('transactions/', views.transactions, name='transactions'),
-    path('transaction/<int:transaction_id>/', views.transaction, name='transaction'),
-]
